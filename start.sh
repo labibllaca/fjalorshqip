43d1c155a1422592299a20215e50545fc9a1512c
LabiMac <labibllaca@gmail.com>
2026-06-24 13:43:04 +0200

    update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ read -p "A po e nisni aplikacionin lokal apo në server? (1/2): " choice
 if [ "$choice" = "1" ]; then
   npm rebuild better-sqlite3 && npm run dev
 elif [ "$choice" = "2" ]; then
-  gitpastro && npm rebuild better-sqlite3 && npm run build && pmr fjalor
+  $gitpastro && npm rebuild better-sqlite3 && npm run build && pmr fjalor
 else
   echo "Zgjedhje e pavlefshme. Shkruani '1' ose '2'."
   exit 1
