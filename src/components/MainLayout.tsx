c535dbf2ae564da00d29d7f39d1b966e78ac1b30
LabiMac <labibllaca@gmail.com>
2026-06-30 12:23:27 +0200

    mobile entryCard padding, page titles for /sot and /spiel, mobile drawer overflow fix

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -35,7 +35,7 @@ const MainLayout = () => {
       <div className="content">
         <header className="content-header">
           <div className="title-row">
-            <a href="/" className="site-title">{pathname === '/fav' ? 'Fjal deshiruese' : 'Fjalor'}</a>
+            <a href="/" className="site-title">{pathname === '/fav' ? 'Fjal deshiruese' : pathname === '/sot' ? 'Fjala e ditës' : pathname === '/spiel' ? 'Gjeje Fjalen' : 'Fjalor'}</a>
             <button
               className="drawer-toggle"
               onClick={toggleDrawer}
